53582d9de5429aef6793dff469b5b3d2bad1f8ee
Tristan Leiter <tristan.leiter@hinder-asset.ch>
2025-11-07 15:44:09 +0100

    Setting up some basic code for exploratory data analysis.

--- 01_Code/Code_Main.R
+++ 01_Code/Code_Main.R
@@ -19,7 +19,8 @@ Path <- file.path(here::here("")) ## You need to install the package first incas
 
 ## Needs to enable checking for install & if not then autoinstall.
 
-packages <- c("here")
+packages <- c("here", "corrplot", "dplyr", "tidyr",
+              "reshape2", "ggplot2")
 
 for(i in 1:length(packages)){
   package_name <- packages[i]
@@ -40,6 +41,15 @@ for(i in 1:length(packages)){
 
 #==== 1B - Functions ==========================================================#
 
+## Skewness.
+skew <- function(x) {
+  x <- na.omit(x)
+  n <- length(x)
+  mean_x <- mean(x)
+  sd_x <- sd(x)
+  skewness_value <- (n / ((n - 1) * (n - 2))) * sum(((x - mean_x) / sd_x)^3)
+  return(skewness_value)
+}
 
 #==== 1C - Parameters =========================================================#
 
@@ -54,6 +64,8 @@ grey <- "#708090"
 orange <- "#F37021"
 red <- "#B22222"
 
+height <- 3750
+width <- 1833
 
 #==============================================================================#
 #==== 02 - Data ===============================================================#
@@ -64,9 +76,82 @@ red <- "#B22222"
 Data <- load(Data_Directory)
 Data <- d
 
+Exclude <- c("id", "refdate", "size","sector", "y")
+Features <- Data[, -which(names(Data) %in% Exclude)]
+
 #==== 02b - Exploratory Data Analysis =========================================#
 
-##
+## ======================= ##
+## First let us check if we have any missing data.
+## ======================= ##
+colSums(is.na(Data)) ## We fine some NAs, mostly in the financial ratio's.
+
+## ======================= ##
+## Check the scale of the features.
+## ======================= ##
+summary(Data)
+
+## ======================= ##
+## Now check our independent variable: loan default.
+## ======================= ##
+table(Data$y) ## 0.865% of all loans are defaulting.
+
+#=======================#
+## Check for data variability and skewed data.
+#=======================#
+hist(Data$f8)
+
+## Check the variation of each feature.
+Variance <- apply(as.matrix(Features), MARGIN = 2, FUN = var)
+Skewness <- apply(as.matrix(Features), MARGIN = 2, FUN = skew)
+
+## We should standardize as the variance differs considerably between our features.
+
+#=======================#
+## Standardize the features.
+#=======================#
+# scaled_predictors <- data.frame(scale(Features))
+# Data_std <- cbind(scaled_predictors, data$quality)
+# colnames(Data_std) <- colnames(Data)
+
+#==== 02c - Multicollinearity =================================================#
+
+## Parameters.
+Path <- file.path(Charts_Directory, "01_Correlation_Plot.png")
+
+## Main Code.
+cor_matrix <- cor(Features[,])
+cor_matrix_upper_na <- cor_matrix
+cor_matrix_upper_na[upper.tri(cor_matrix_upper_na)] <- NA
+melted_cor_matrix <- melt(cor_matrix_upper_na, na.rm = TRUE)
+
+## Plot.
+plot <- ggplot(data = melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile(color = "white") + 
+  scale_fill_gradient2(low = blue, high = orange, mid = "white", 
+                       midpoint = 0, limit = c(-1, 1), 
+                       name = "Correlation") +
+  geom_text(aes(label = round(value, 2)), color = "black", size = 1.5) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+        axis.title = element_blank()) + 
+  coord_fixed() 
+
+ggsave(
+  filename = Path,
+  plot = plot,
+  width = height,
+  height = width,
+  units = "px",
+  dpi = 300,
+  limitsize = FALSE
+)
+
+#==== 02d - Splitting the dataset =============================================#
+## 50/25/25 rule - train, validation, test
+set.seed(123)
+
+
 
 
 
