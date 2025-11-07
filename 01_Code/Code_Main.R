22df5d513840c13c774397736aea918baa216b6a
Tristan Leiter <tristan.leiter@hinder-asset.ch>
2025-11-07 18:53:46 +0100

    Updated the GLM and Regularized GLM code. Only using the financial metrics for now.

--- 01_Code/Code_Main.R
+++ 01_Code/Code_Main.R
@@ -23,7 +23,8 @@ packages <- c("here", "corrplot", "dplyr", "tidyr",
               "reshape2", "ggplot2",
               "rsample", "DataExplorer",  ## Necessary for stratified sampling.
               "pROC",                     ## Area under the Curve (AuC) measure.
-              "caret"                     ## GLM.
+              "caret",                    ## GLM.
+              "glmnet"                    ## Regularized regression
               )
 
 for(i in 1:length(packages)){
@@ -84,6 +85,10 @@ Exclude <- c("id", "refdate", "size","sector", "y")
 Features <- Data[, -which(names(Data) %in% Exclude)]
 
 Exclude <- c("id", "refdate") ## Drop the id and ref_date (year) for now.
+Data <- Data[, -which(names(Data) %in% Exclude)]
+
+## Drop all ratios for now.
+Exclude <- c(paste("r", seq(1:18), sep = "")) ## Drop all ratios for now.
 Data <- Data[, -which(names(Data) %in% Exclude)]
 
 #==== 02b - Exploratory Data Analysis =========================================#
@@ -242,16 +247,31 @@ median_values <- Validation %>%
   summarise(across(all_of(cols_to_impute), ~median(., na.rm = TRUE))) %>%
   as.list()
 
-# Validation <- Validation %>%
-#   mutate(across(all_of(cols_to_impute), 
-#                 ~coalesce(., median_values[[cur_column()]])))
+Validation <- Validation %>%
+  mutate(
+    across(all_of(cols_to_impute), 
+           ~ifelse(is.na(.), 1, 0), 
+           .names = "{.col}_is_missing"),
+    across(all_of(cols_to_impute), 
+           ~coalesce(., median_values[[cur_column()]]))
+  )
 
 ## ======================= ##
 ## Test data.
 ## ======================= ##
+median_values <- Test %>%
+  summarise(across(all_of(cols_to_impute), ~median(., na.rm = TRUE))) %>%
+  as.list()
+
 Test <- Test %>%
-  mutate(across(all_of(cols_to_impute), 
-                ~coalesce(., median_values[[cur_column()]])))
+  mutate(
+    across(all_of(cols_to_impute), 
+           ~ifelse(is.na(.), 1, 0), 
+           .names = "{.col}_is_missing"),
+    across(all_of(cols_to_impute), 
+           ~coalesce(., median_values[[cur_column()]]))
+  )
+
 
 #==============================================================================#
 #==== 03 - Setting up the Loss-function =======================================#
@@ -266,6 +286,8 @@ Test <- Test %>%
 
 #==== 04a - Binary regression model (log) =====================================#
 
+tryCatch({
+  
 ## ======================= ##
 ## Parameters.
 ## ======================= ##
@@ -282,6 +304,9 @@ summary(model_logit)
 ## ======================= ##
 ## Optimize for AuC.
 ## ======================= ##
+## Here we could consider plotting the true positive rate vs the false positive rate.
+## Easier to mentally process than sensitivity and specificity.
+
 pred_prob_val <- predict(model_logit, newdata = Validation, type = "response")
 roc_obj <- roc(Validation$y, pred_prob_val)
 
@@ -291,22 +316,29 @@ print(paste("Validation AUC:", round(auc_val, 4)))
 plot(roc_obj, main = "ROC Curve - Logistic Regression")
 
 ## ======================= ##
-## Recall (threshhold for 95% recall).
+## Recall.
 ## ======================= ##
-best_threshold_coords <- coords(roc_obj, 
-                                x = 0.95,
-                                input = "sensitivity", 
-                                ret = "threshold")
+best_coords_topleft <- coords(roc_obj, 
+                              x = "best", 
+                              best.method = "closest.topleft", 
+                              ret = c("threshold", "sensitivity", "specificity", "precision"))
 
-best_threshold <- best_threshold_coords$threshold
+print("--- Optimal Threshold (Closest to Top-Left) ---")
+print(best_coords_topleft)
 
-print(paste("Threshold for 95% Recall:", round(best_threshold, 4)))
+best_coords_youden <- coords(roc_obj, 
+                             x = "best", 
+                             best.method = "youden", 
+                             ret = c("threshold", "sensitivity", "specificity", "precision"))
+
+print("--- Optimal Threshold (Youden Index) ---")
+print(best_coords_youden)
 
 ## ======================= ##
 ## Evaluation on the test set.
 ## ======================= ##
-pred_prob_test <- predict(model_logit, newdata = test_data, type = "response")
-pred_class_test <- as.factor(ifelse(pred_prob_test > best_threshold, "1", "0"))
+pred_prob_test <- predict(model_logit, newdata = Test, type = "response")
+pred_class_test <- as.factor(ifelse(pred_prob_test > best_coords_youden$threshold, "1", "0"))
 Test$y <- as.factor(Test$y)
 
 final_report <- confusionMatrix(data = pred_class_test, 
@@ -316,8 +348,84 @@ final_report <- confusionMatrix(data = pred_class_test,
 print("Final Model Report on Test Set:")
 print(final_report)
 
+}, silent = TRUE)
 
+#==== 04b - Regularized regression model (log) ================================#
 
+tryCatch({
+
+## ======================= ##
+## Parameters.
+## ======================= ##
+x_train <- model.matrix(y ~ . -1, data = Train)
+y_train <- Train$y
+
+x_val <- model.matrix(y ~ . -1, data = Validation)
+y_val <- Validation$y
+
+x_test <- model.matrix(y ~ . -1, data = Test)
+y_test <- Test$y
+
+## ======================= ##
+## Model training.
+## ======================= ##
+cv_model_logit <- cv.glmnet(x_train, 
+                            y_train, 
+                            family = "binomial", 
+                            alpha = 1)
+
+## ======================= ##
+## Optimize for AuC.
+## ======================= ##
+pred_prob_val <- predict(cv_model_logit, 
+                         newx = x_val, 
+                         s = "lambda.min", 
+                         type = "response")
+
+roc_obj <- roc(y_val, pred_prob_val)
+auc_val <- auc(roc_obj)
+print(paste("Validation AUC:", round(auc_val, 4)))
+
+plot(roc_obj, main = "ROC Curve - glmnet (Lasso)")
+
+## ======================= ##
+## Recall (threshhold for 95% recall).
+## ======================= ##
+best_coords_topleft <- coords(roc_obj, 
+                              x = "best", 
+                              best.method = "closest.topleft", 
+                              ret = c("threshold", "sensitivity", "specificity", "precision"))
+
+print("--- Optimal Threshold (Closest to Top-Left) ---")
+print(best_coords_topleft)
+
+best_coords_youden <- coords(roc_obj, 
+                             x = "best", 
+                             best.method = "youden", 
+                             ret = c("threshold", "sensitivity", "specificity", "precision"))
+
+print("--- Optimal Threshold (Youden Index) ---")
+print(best_coords_youden)
+
+## ======================= ##
+## Evaluation on the test set.
+## ======================= ##
+pred_prob_test <- predict(cv_model_logit, 
+                          newx = x_test, 
+                          s = "lambda.min", 
+                          type = "response")
+
+pred_class_test <- as.factor(ifelse(pred_prob_test > best_coords_youden$threshold, "1", "0"))
+y_test_factor <- as.factor(y_test)
+
+final_report <- confusionMatrix(data = pred_class_test, 
+                                reference = y_test_factor,
+                                positive = "1")
+
+print("Final Model Report on Test Set:")
+print(final_report)
+
+}, silent = TRUE)
 
 #==============================================================================#
 #==============================================================================#
